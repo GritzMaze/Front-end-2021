14955fabb5bceb46b5018b68dfc9aa29f59b56dd
GritzMaze <jivkovvesselin@gmail.com>
2021-12-22 23:15:03 +0200

    Added required resources

---
+++ homework-3/script.js
@@ -0,0 +1,12 @@
+import "https://unpkg.com/@ui5/webcomponents-fiori@1.0.2/dist/ShellBar.js?module";
+import "https://unpkg.com/@ui5/webcomponents@1.0.2/dist/Panel.js?module";
+import "https://unpkg.com/@ui5/webcomponents@1.0.2/dist/Input.js?module";
+import "https://unpkg.com/@ui5/webcomponents@1.0.2/dist/List.js?module";
+import "https://unpkg.com/@ui5/webcomponents@1.0.2/dist/Dialog.js?module";
+import "https://unpkg.com/@ui5/webcomponents@1.0.2/dist/CustomListItem.js?module";
+import "https://unpkg.com/@ui5/webcomponents@1.0.2/dist/StandardListItem.js?module";
+import "https://unpkg.com/@ui5/webcomponents@1.0.2/dist/Table.js?module";
+import "https://unpkg.com/@ui5/webcomponents@1.0.2/dist/TableColumn.js?module";
+import "https://unpkg.com/@ui5/webcomponents@1.0.2/dist/TableRow.js?module";
+import "https://unpkg.com/@ui5/webcomponents@1.0.2/dist/TableCell.js?module";
+import "https://unpkg.com/@ui5/webcomponents-icons@1.0.2/dist/hint.js?module";
